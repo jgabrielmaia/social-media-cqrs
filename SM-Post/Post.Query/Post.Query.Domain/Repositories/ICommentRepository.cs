c71b6a2ea26a65a6174c72b0656c23504441f754
Jonas Maia <ext.jonas.maia@farfetch.com>
2023-08-03 19:57:58 +0100

    CQRS: Creating Comment Repository

--- SM-Post/Post.Query/Post.Query.Domain/Repositories/ICommentRepository.cs
+++ SM-Post/Post.Query/Post.Query.Domain/Repositories/ICommentRepository.cs
@@ -4,8 +4,8 @@ namespace Post.Query.Domain.Repositories;
 
 public interface ICommentRepository
 {
-    Task CreateAsync(CommentEntity post);
-    Task UpdateAsync(CommentEntity post);
-    Task GetByIdAsync(Guid postId);
-    Task DeleteAsync(Guid postId);
+    Task CreateAsync(CommentEntity comment);
+    Task UpdateAsync(CommentEntity comment);
+    Task<CommentEntity> GetByIdAsync(Guid commentId);
+    Task DeleteAsync(Guid commentId);
 }
